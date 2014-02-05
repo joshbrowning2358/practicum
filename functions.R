9a3d2b16922b0e23b303f4f15bef61ae08ddd1c3
rockclimber112358 <rockclimber112358@gmail.com>
2014-02-05 04:55:10 -0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -12,6 +12,7 @@ library(reshape)
 library(plyr)
 library(ggplot2)
 library(scales)
+library(neuralnet)
 
 #form: specify the model formula, i.e. Y ~ X1 + X2 + X3.  Note that "." notation is supported.
 #data: a dataframe containing the data for which the model is desired.
