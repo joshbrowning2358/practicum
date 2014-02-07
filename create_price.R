0e38eac9b30a7370dbd8d951f1d8e05300b89006
rockclimber112358 <rockclimber112358@gmail.com>
2014-02-07 16:55:08 -0700

    Update create_price.R

--- create_price.R
+++ create_price.R
@@ -48,48 +48,29 @@ price$OfferLow4Cnt = apply(price[,6:10*3-1]*(abs(price[,6:10*3+1]-price$OfferPri
 price$OfferLow5Cnt = apply(price[,6:10*3-1]*(abs(price[,6:10*3+1]-price$OfferPrice1-.04)<.005),1,sum, na.rm=T)
 
 form = paste("PriceDiff1SecAhead ~", paste0("BidHigh",1:5,"Cnt", collapse=" + "),"+",paste0("OfferLow",1:5,"Cnt", collapse=" + "))
+
+#cvGroup = sample(1:10, size=nrow(price), replace=T)
+#fit = cvModel( d=price, cvGroup, indCol=35, model=paste("glm(", form, ")" ) )
+#coeffs = data.frame( do.call("rbind", fit$models) )
+#coeffs = melt(coeffs, measure.vars=1:ncol(coeffs) )
+#ggplot( coeffs, aes(x=variable, y=value) ) + geom_point() +
+#  geom_boxplot()
+#Coefficients are very consistent across CV groups (except for intercept).  Thus, it should be safe to use the model without worrying about it skewing cross-validation results.
+
 fit = glm( as.formula(form), data=price)
 price$MicroPriceAdj1Sec = predict(fit)
 price$MicroPriceAdj1Sec = price$MicroPriceAdj1Sec + price$MicroPrice
-#form = paste("PriceDiff60SecAhead ~", paste0("BidHigh",1:5,"Cnt", collapse=" + "),"+",paste0("OfferLow",1:5,"Cnt", collapse=" + "))
-#fit = glm( as.formula(form), data=price)
-#price$MicroPriceAdj60Sec = predict(fit)
-
-###############################################################################
-# If desired, check cross-validated performance:
-###############################################################################
-
-#form = paste("PriceDiff1SecAhead ~", paste0("BidHigh",1:5,"Cnt", collapse=" + "),"+",paste0("OfferLow",1:5,"Cnt", collapse=" + "))
-#cvGroup = sample(c(-1,1:10),size=nrow(price),replace=T)
-#fit.bench = cvModel(price, cvGroup, model="glm(MicroPrice1SecAhead ~ MicroPrice)", indCol=33 )
-#fit.new = cvModel(price, cvGroup, model=paste("glm(",form,")"), indCol=33 )
-##Convert fit.new to appropriate scale:
-#fit.new = fit.new + price$MicroPrice
-##Wahoo! This idea beats the persistence model and a regression model against current microprice!
-#sum( (price$MicroPrice - price$MicroPrice1SecAhead)[cvGroup==-1]^2 )
-#sum( (fit.bench - price$MicroPrice1SecAhead)[cvGroup==-1,]^2 )
-#sum( (fit.new - price$MicroPrice1SecAhead)[cvGroup==-1,]^2, na.rm=T )
-
-#form = paste("PriceDiff60SecAhead ~", paste0("BidHigh",1:5,"Cnt", collapse=" + "),"+",paste0("OfferLow",1:5,"Cnt", collapse=" + "))
-#cvGroup = sample(c(-1,1:10),size=nrow(price),replace=T)
-#fit.bench = cvModel(price, cvGroup, model="glm(MicroPrice60SecAhead ~ MicroPrice)", indCol=33 )
-#fit.new = cvModel(price, cvGroup, model=paste("glm(",form,")"), indCol=33 )
-##Convert fit.new to appropriate scale:
-#fit.new = fit.new + price$MicroPrice
-##These models do terrible... Weird...
-#sum( (price$MicroPrice - price$MicroPrice1SecAhead)[cvGroup==-1]^2 )
-#sum( (fit.bench - price$MicroPrice60SecAhead)[cvGroup==-1,]^2 )
-#sum( (fit.new - price$MicroPrice60SecAhead)[cvGroup==-1,]^2, na.rm=T )
+#Note: could also do this for 60 seconds ahead, but you get almost identical coefficients
 
 ###############################################################################
 # Add volume columns
 ###############################################################################
 
-price$TotalBidQuantity = price$BidQuantity1 + price$BidQuantity2 + price$BidQuantity3 + price$BidQuantity4 + price$BidQuantity5
-price$TotalOfferQuantity = price$OfferQuantity1 + price$OfferQuantity2 + price$OfferQuantity3 + price$OfferQuantity4 + price$OfferQuantity5
-colnames(price)[colnames(price)=="BidQuantity1"] = "InsideBidQuantity"
-colnames(price)[colnames(price)=="OfferQuantity1"] = "InsideOfferQuantity"
-price$Spread = price$OfferPrice1 - price$BidPrice1
+#price$TotalBidQuantity = price$BidQuantity1 + price$BidQuantity2 + price$BidQuantity3 + price$BidQuantity4 + price$BidQuantity5
+#price$TotalOfferQuantity = price$OfferQuantity1 + price$OfferQuantity2 + price$OfferQuantity3 + price$OfferQuantity4 + price$OfferQuantity5
+#colnames(price)[colnames(price)=="BidQuantity1"] = "InsideBidQuantity"
+#colnames(price)[colnames(price)=="OfferQuantity1"] = "InsideOfferQuantity"
+#price$Spread = price$OfferPrice1 - price$BidPrice1
 
 #Remove unneccesary columns:
 rmCols = c(paste0("BidQuantity",1:5), paste0("BidNumberOfOrders",1:5), paste0("BidPrice",1:5)
@@ -97,54 +78,50 @@ rmCols = c(paste0("BidQuantity",1:5), paste0("BidNumberOfOrders",1:5), paste0("B
           ,paste0("BidHigh",1:5,"Cnt"), paste0("OfferLow",1:5,"Cnt"))
 for(i in rmCols) price[,i] = NULL
 
-#write.csv(price, file="price.csv")
-#price = read.csv(file="price.csv")
-#price$Time = as.POSIXct(strptime(x=price$Time, format="%Y%m%d %H:%M:%OS"))
-
 ###############################################################################
 # Add Lagged Time Variables and estimated derivatives
 ###############################################################################
 
-lag = load_lag_times(as.matrix(price[,c("Time","MicroPriceAdj1Sec")]))
-colnames(lag) = paste0("MicroPriceAdj1Sec_Lag_",1:20,"s")
+lag = load_lag_price(price[,c("Time","MicroPriceAdj1Sec")], lags=c(1:30,45,60,120,300,600))
+price = cbind(price, lag)
+
+lag = load_lag_price(price[,c("Time","MicroPrice")], lags=c(1:30,45,60,120,300,600))
 price = cbind(price, lag)
 rm(lag)
 
-lag = load_lag_times(as.matrix(price[,c("Time","MicroPrice")]))
-colnames(lag) = paste0("MicroPrice_Lag_",1:20,"s")
-price = cbind(price, lag)
-rm(lag)
-
+#####Skipping derivatives because they'll create a linearly dependent matrix:#####
 #See http://en.wikipedia.org/wiki/Finite_difference_coefficients
-deriv_mat = matrix(0,nrow=6,ncol=4)
-deriv_mat[,1] = c(-137/60, 5, -5, 10/3, -5/4, 1/5)
-deriv_mat[,2] = c(15/4, -77/6, 107/6, -13, 61/12, -5/6)
-deriv_mat[,3] = c(-17/4, 71/4, -59/2, 49/2, -41/4, 7/4)
-deriv_mat[,4] = c(3, -14, 26, -24, 11, -2)
-derivs = as.matrix(price[,c("MicroPriceAdj1Sec",paste0("MicroPriceAdj1Sec_Lag_",1:5,"s"))]) %*% deriv_mat
-colnames(derivs) = paste0("DerivAdj",1:4)
-price = cbind(price, derivs)
-rm(derivs)
+#deriv_mat = matrix(0,nrow=6,ncol=4)
+#deriv_mat[,1] = c(-137/60, 5, -5, 10/3, -5/4, 1/5)
+#deriv_mat[,2] = c(15/4, -77/6, 107/6, -13, 61/12, -5/6)
+#deriv_mat[,3] = c(-17/4, 71/4, -59/2, 49/2, -41/4, 7/4)
+#deriv_mat[,4] = c(3, -14, 26, -24, 11, -2)
+#derivs = as.matrix(price[,c("MicroPriceAdj1Sec",paste0("MicroPriceAdj1Sec_Lag_",1:5,"s"))]) %*% deriv_mat
+#colnames(derivs) = paste0("DerivAdj",1:4)
+#price = cbind(price, derivs)
+#rm(derivs)
 
-derivs = as.matrix(price[,c("MicroPrice",paste0("MicroPrice_Lag_",1:5,"s"))]) %*% deriv_mat
-colnames(derivs) = paste0("Deriv",1:4)
-price = cbind(price, derivs)
-rm(derivs)
+#derivs = as.matrix(price[,c("MicroPrice",paste0("MicroPrice_Lag_",1:5,"s"))]) %*% deriv_mat
+#colnames(derivs) = paste0("Deriv",1:4)
+#price = cbind(price, derivs)
+#rm(derivs)
 
 ###############################################################################
-# Add Time Variables
+# Add Time Variables: Seem pretty useless, so skipping these too.
 ###############################################################################
 
-price$SecSinceOpen = price$Time %% (24*3600)
-price$SecSinceHour = price$Time %% 3600
-price$SecSinceMin = price$Time %% 60
+#price$SecSinceOpen = price$Time %% (24*3600)
+#price$SecSinceHour = price$Time %% 3600
+#price$SecSinceMin = price$Time %% 60
 
 ###############################################################################
 # Trade History
 ###############################################################################
 
-trade_hist = load_lag_trades( price, orders, lag=60 )
-price = cbind( price, trade_hist )
+for( lag in c(1:30,45,60,120,300,600) ){
+  trade_hist = load_lag_trades( price, orders, lag=lag )
+  price = cbind( price, trade_hist )
+}
 
 write.csv( price, file="/home/josh/Documents/Professional Files/Mines/MATH 598- Statistics Practicum/price.csv", row.names=F )
 write.csv( file="C:/Users/jbrowning/Desktop/To Home/Personal/Mines Files/MATH 598- Statistics Practicum/Data/price.csv", price, row.names=F )
