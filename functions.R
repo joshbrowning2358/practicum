3a8e7d69fc71985070ce48bf9c9af4050e5c67e6
rockclimber112358 <rockclimber112358@gmail.com>
2014-03-21 13:07:28 -0600

    eval_preds fixes
    Return RMSE instead of MSE, include additional arg for speed.

--- functions.R
+++ functions.R
@@ -17,7 +17,7 @@ library(biglm)
 library(bigmemory)
 library(sqldf)
 
-eval_preds = function( preds, price_diff=d[,5], price=d[,2], time=d[,1] ){
+eval_preds = function( preds, price_diff=d[,5], price=d[,2], time=d[,1], full=F ){
   if( any(is.na(preds)) ){
     warning(paste0("No NAs allowed in predictions!  ",sum(is.na(preds))," NA's replaced with MicroPrice at that time."))
     preds[is.na(preds)] = price[is.na(preds)]
@@ -27,26 +27,30 @@ eval_preds = function( preds, price_diff=d[,5], price=d[,2], time=d[,1] ){
   d.eval = data.frame( err=preds-price_diff, time, price, price_diff )
   d.eval = d.eval[eval.rows,]
 
-  #Aggregate performance over time:
-  d.eval$time = floor( d.eval$time/15/60 )*15*60
-  d.agg.t = ddply( d.eval, "time", function(df){
-    SS = sum( (df$err)^2 )
-    data.frame(#Base.MSE=sum(df$price_diff^2)/nrow(df),
-      Model.MSE = SS/nrow(df) )
-  } )
-
-  #Aggregate performance over actual MicroPrice:
-  d.eval$price = floor( d.eval$price*100 )/100
-  d.agg.p = ddply( d.eval, "price", function(df){
-    SS = sum( (df$err)^2 )
-    data.frame(#Base.MSE=sum(df$price_diff^2)/nrow(df),
-      Model.MSE = SS/nrow(df) )
-  } )
-  
   d.agg.tot = sum(d.eval$err^2)/nrow(d.eval)
+
+  if(full){
+    #Aggregate performance over time:
+    d.eval$time = floor( d.eval$time/15/60 )*15*60
+    d.agg.t = ddply( d.eval, "time", function(df){
+      SS = sum( (df$err)^2 )
+      data.frame(#Base.MSE=sum(df$price_diff^2)/nrow(df),
+        Model.RMSE = sqrt(SS/nrow(df)) )
+    } )
+
+    #Aggregate performance over actual MicroPrice:
+    d.eval$price = floor( d.eval$price*100 )/100
+    d.agg.p = ddply( d.eval, "price", function(df){
+      SS = sum( (df$err)^2 )
+      data.frame(#Base.MSE=sum(df$price_diff^2)/nrow(df),
+        Model.RMSE = sqrt(SS/nrow(df)) )
+    } )
   
-  #return performance aggregated by time and by current price
-  return( list( d.agg.tot, d.agg.t, d.agg.p ) )
+    #return performance aggregated by time and by current price
+    return( list( sqrt(d.agg.tot), d.agg.t, d.agg.p ) )
+  }
+  
+  return(sqrt(d.agg.tot))
 }
 
 eval_print = function( preds, price_diff=d[,5], price=d[,2], time=d[,1] ){
